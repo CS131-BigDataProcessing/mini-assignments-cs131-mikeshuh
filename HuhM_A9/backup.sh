94c74d01e09b2a9313eef9d65f62a359551eb56a
Michael Huh <michael.stephen.huh@gmail.com>
2024-10-21 17:42:26 -0700

    HuhM_A9

---
+++ HuhM_A9/backup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+tar -czf /Users/michaelhuh/Documents/SJSU/24-25/FA_24/CS-131_big-data-processing/backups/CS-131_backup_$(date +\%Y\%m\%d).tar.gz /Users/michaelhuh/Documents/SJSU/24-25/FA_24/CS-131_big-data-processing
+
