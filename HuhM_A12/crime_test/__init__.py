2476f575d2df4f14cfaabae64cb433f312757c73
Michael Huh <michael.stephen.huh@gmail.com>
2024-12-04 02:11:36 -0800

    HuhM_A12

--- HuhM_A12/crime_test/__init__.py
+++ HuhM_A12/crime_test/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '1.0'
-from stats_function import age_mean, age_median
-from validate_functions import validate_victim_sex, validate_victim_age
+from .stats_function import age_mean, age_median
+from .validate_functions import validate_victim_sex, validate_victim_age
 print("Thank you for installing this package.")
--- HuhM_A12/setup.py
+++ HuhM_A12/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='crime_dataset_test',                      # Package name
+    name='crime_test',                              # Package name
     version='1.0',                                  # Package version
     packages=find_packages(),                       # Automatically find modules
     install_requires=[
